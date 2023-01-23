109d7511dfb7e0f5c1f9165c1549b7a5cc23ec07
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-01-23 11:35:26 -0300

    refactor: changing from single quotes to double quotes in article listing

--- src/presentation/pages/home/components/item/item.tsx
+++ src/presentation/pages/home/components/item/item.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { LoadArticleList } from '@/domain/usecases'
-import { CategoryModel } from '@/domain/models'
+import React from "react"
+import { LoadArticleList } from "@/domain/usecases"
 import {
   Box,
   Heading,
@@ -9,8 +8,8 @@ import {
   useColorModeValue,
   WrapItem,
   Link as LinkChakra,
-} from '@chakra-ui/react'
-import { Tags } from '@/presentation/components'
+} from "@chakra-ui/react"
+import { Tags } from "@/presentation/components"
 
 type ArticleItemProps = {
   article: LoadArticleList.Model
@@ -18,39 +17,41 @@ type ArticleItemProps = {
 
 const ArticleItem: React.FC<ArticleItemProps> = ({ article }: ArticleItemProps) => {
   return (
-    <>
-      <WrapItem maxW='374px' flexDirection="row" bg={useColorModeValue('white', 'gray.900')}
-        boxShadow='2xl'>
-        <Box w="100%">
-          <Box borderRadius="lg" overflow="hidden">
-            <LinkChakra textDecoration="none" _hover={{ textDecoration: 'none' }}>
-              <Image
-                transform="scale(1.0)"
-                src={article.featured_media.medium_large}
-                alt="some text"
-                objectFit="contain"
-                width="100%"
-                transition="0.3s ease-in-out"
-                _hover={{
-                  transform: 'scale(1.05)',
-                }}
-              />
-            </LinkChakra>
-          </Box>
-          <Box p={3}>
-            <Tags tags={article.categories.map(c => c.name)} marginTop="3" />
-            <Heading fontSize="xl" marginTop="2">
-              <LinkChakra textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                {article.title}
-              </LinkChakra>
-            </Heading>
-            <Text as="p" fontSize="md" marginTop="2" color='gray.500'>
-              {article.headline}
-            </Text>
-          </Box>
+    <WrapItem
+      maxW="370px"
+      flexDirection="row"
+      bg={useColorModeValue("white", "gray.900")}
+      boxShadow="lg"
+    >
+      <Box w="100%">
+        <Box borderRadius="lg" overflow="hidden">
+          <LinkChakra textDecoration="none" _hover={{ textDecoration: "none" }}>
+            <Image
+              transform="scale(1.0)"
+              src={article.featured_media.medium_large}
+              alt={article.title}
+              objectFit="contain"
+              width="100%"
+              transition="0.3s ease-in-out"
+              _hover={{
+                transform: "scale(1.05)",
+              }}
+            />
+          </LinkChakra>
         </Box>
-      </WrapItem>
-    </>
+        <Box px={5} py={1} pb={3}>
+          <Tags tags={article.categories.map(c => c.name)} marginTop="3" />
+          <Heading fontSize="xl" marginTop="2">
+            <LinkChakra textDecoration="none" _hover={{ textDecoration: "none" }}>
+              {article.title}
+            </LinkChakra>
+          </Heading>
+          <Text as="p" fontSize="md" marginTop="2" color="gray.500">
+            {article.headline}
+          </Text>
+        </Box>
+      </Box>
+    </WrapItem>
   )
 }
 
