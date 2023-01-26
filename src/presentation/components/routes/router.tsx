4b8b1390820b30358818373c9a0aa61f9f4d0b91
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-01-26 16:31:13 -0300

    feat: redirecting from route / to home

--- src/presentation/components/routes/router.tsx
+++ src/presentation/components/routes/router.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import { makeArticleList, makeArticle } from "@/main/factories/pages"
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={makeArticleList({})} />
         <Route path="/article/:id" element={makeArticle({})} />
       </Routes>
