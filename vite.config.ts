7bd5f392f04c2d3c9b538c6b179a93d752fae91b
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-01-28 17:11:38 -0300

    chore: configuring variables in build of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
 
 const root = path.resolve(__dirname, "./src");
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@/*": path.resolve(root, "*"),
+export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const env = loadEnv(mode, process.cwd())
+
+  const processEnvValues = {
+    'process.env': Object.entries(env).reduce(
+      (prev, [key, val]) => {
+        return {
+          ...prev,
+          [key]: val,
+        }
+      },
+      {},
+    )
+  }
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: processEnvValues,
+    resolve: {
+      alias: {
+        "@/*": path.resolve(root, "*"),
+      },
     },
-  },
+  }
 });
